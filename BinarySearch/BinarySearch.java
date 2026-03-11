253f284ade193f30b7032384081c1b0af6664ef7
Vicky Daniel <vicky16898@gmail.com>
2026-03-11 02:08:31 -0400

    add binary search

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (target == nums[mid])
+                return mid;
+            else if (target > nums[mid])
+                left++;
+            else
+                right--;
+        }
+        return -1;
+    }
+}
