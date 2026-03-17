cac38e567bdc96016f8e5782b1d5cf27c95fb77a
Vicky Daniel <vicky16898@gmail.com>
2026-03-17 17:53:52 -0400

    add max substring with character replacement

---
+++ SlidingWindow/MaxSubstringWithCharacterReplacement.java
@@ -0,0 +1,18 @@
+public class MaxSubstringWithCharacterReplacement {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxFreq = 0;
+        int maxLength = 0;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            maxFreq = Math.max(maxFreq, map.get(s.charAt(i)));
+            while (i - left + 1 - maxFreq > k) {
+                map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+                left++;
+            }
+            maxLength = i - left + 1;
+        }
+        return maxLength;
+    }
+}
