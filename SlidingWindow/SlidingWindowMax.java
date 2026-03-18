4bc29b463f9e11bf4969ae728d243f9d5c59d639
Vicky Daniel <vicky16898@gmail.com>
2026-03-18 14:21:54 -0400

    add sliding window max

---
+++ SlidingWindow/SlidingWindowMax.java
@@ -0,0 +1,25 @@
+package SlidingWindow;
+
+public class SlidingWindowMax {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] res = new int[nums.length - k + 1];
+
+        int left = 0;
+        int counter = 0;
+        Deque<Integer> dq = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (dq.size() > 0 && nums[dq.getLast()] < nums[i]) {
+                dq.removeLast();
+            }
+            dq.addLast(i);
+            if (left > dq.getFirst())
+                dq.removeFirst();
+            if (i - left + 1 == k) {
+                res[counter] = nums[dq.getFirst()];
+                left++;
+                counter++;
+            }
+        }
+        return res;
+    }
+}
