7e5d4e70e988c2e2af633aa18a5acf889c87abf9
Vicky Daniel <vicky16898@gmail.com>
2026-03-24 16:00:29 -0400

    fix compiler error issues

--- Tree/InvertTree.java
+++ Tree/InvertTree.java
@@ -11,3 +11,22 @@ public class InvertTree {
         return root;
     }
 }
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
